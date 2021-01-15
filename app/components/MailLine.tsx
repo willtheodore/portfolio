da01e590d1860c445bb0c06566ec6d1387b06647
Will Theodore <willtheodore@comcast.net>
2021-01-14 22:13:44 -0500

    FEATURE: added mobile support to contact page

--- app/components/MailLine.tsx
+++ app/components/MailLine.tsx
@@ -1,4 +1,4 @@
-import { Flex, chakra, Text } from "@chakra-ui/react";
+import { Flex, chakra, Text, useBreakpointValue } from "@chakra-ui/react";
 import * as React from "react";
 import ThemeContext from "../contexts/ThemeContext";
 import mail from "../assets/mail.svg";
@@ -9,22 +9,30 @@ interface MailLineProps {
 
 export default function MailLine({ children }: MailLineProps) {
 	const theme = React.useContext(ThemeContext);
+	const isMobile = useBreakpointValue({ base: true, md: false });
 
 	return (
 		<Flex
-			className="single-line uses-color-theme"
-			ml="-200px"
-			p="20px 100px 20px 222px"
+			className={`${!isMobile && "single-line"} uses-color-theme`}
+			display="inline-block"
+			ml={{ base: "", md: "-200px" }}
+			p={{ base: "20px", md: "20px 100px 20px 222px" }}
 			direction="row"
+			w={isMobile ? "100%" : ""}
 			alignItems="center"
-			w="70%"
 			bg={theme}
 		>
-			<chakra.img src={mail} h="26px" mr="20px " display="inline" />
+			<chakra.img
+				src={mail}
+				h={{ base: "20px", md: "2vw" }}
+				mr="20px "
+				display="inline"
+			/>
 			<Text
 				as="a"
 				href={`mailto:${children}`}
 				textStyle="header"
+				fontSize={{ base: "20px", md: "2vw" }}
 				_hover={{ textDecoration: "underline" }}
 			>
 				{children}
--- app/components/SingleLine.tsx
+++ app/components/SingleLine.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, useBreakpointValue } from "@chakra-ui/react";
 import * as React from "react";
 
 interface SingleLineProps {
@@ -6,13 +6,15 @@ interface SingleLineProps {
 }
 
 export default function SingleLine({ children }: SingleLineProps) {
+	const isMobile = useBreakpointValue({ base: true, md: false });
+
 	return (
 		<Box
-			className="single-line"
-			w="75vw"
-			mt="35vh"
-			p="40px 50px 40px 222px"
-			ml="-200px"
+			className={isMobile ? "" : "single-line"}
+			w={{ base: "100%", md: "75vw" }}
+			mt={{ base: "25vh", md: "35vh" }}
+			p={{ base: "20px", md: "40px 50px 40px 222px" }}
+			ml={{ base: "", md: "-200px" }}
 			bg="darkGray"
 		>
 			<Text textStyle="body" fontSize="25px">
