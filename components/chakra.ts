c51f22c9d6938ffef935a1f6db487011b804b944
Will Theodore <willtheodore@comcast.net>
2021-02-16 17:55:08 -0500

    BFX: Removed uneeded import

--- components/chakra.ts
+++ components/chakra.ts
@@ -1,8 +1,6 @@
 import { chakra } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
 import { Footer as RawFooter } from "./Footer";
 import { HeaderWithContext as RawHeader } from "./HeaderWithContext";
 
-export const ChakraLink = chakra(Link);
 export const Footer = chakra(RawFooter);
 export const HeaderWithContext = chakra(RawHeader);
